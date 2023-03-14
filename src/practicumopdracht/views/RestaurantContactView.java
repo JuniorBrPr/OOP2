fbaa76c20e67cc0ed4465578e4c39e9933bde36c
Junior Brito <junior.br.pr@gmail.com>
2023-03-15 00:25:26 +0100

    - Views redo

--- src/practicumopdracht/views/RestaurantContactView.java
+++ src/practicumopdracht/views/RestaurantContactView.java
@@ -8,212 +8,126 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import practicumopdracht.models.RestaurantContact;
+import practicumopdracht.models.RestaurantPhoneBook;
 
 import static java.lang.Integer.MAX_VALUE;
 
 public class RestaurantContactView extends View {
 
-    private TextField nameField;
-    private ComboBox<String> cuisineField;
-    private TextField phoneNumberField;
-    private TextArea addressField;
-    private TextField tablesField;
-    private TextField ratingField;
-    private DatePicker establishedField;
-    private CheckBox wheelchairAccessibleField;
-    private ListView<RestaurantContact> restaurantContactListView;
-    private Button saveButton;
-    private Button deleteButton;
-    private Button newButton;
-    private Button cancelButton;
-
     @Override
     protected void initializeView() {
-        GridPane mainPanel = new GridPane();
-        mainPanel.setPadding(new Insets(10));
-        mainPanel.setAlignment(Pos.CENTER);
-        mainPanel.setHgap(20);
-        mainPanel.setVgap(25);
-
         Font smallFont = Font.font("Helvetica", FontWeight.BLACK, 16);
         Font largeFont = Font.font("Helvetica", FontWeight.BOLD, 36);
 
+        Label titleLabel = new Label("Restaurant Contact");
+        titleLabel.setAlignment(Pos.CENTER);
         HBox titleBox = new HBox();
         titleBox.setAlignment(Pos.TOP_CENTER);
+        titleBox.setPrefHeight(100);
         titleBox.setBorder(new Border(
                 new BorderStroke(
                         Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT
                 )));
-
-        Label titleLabel = new Label("Restaurant Contact");
-        titleLabel.setFont(largeFont);
-        titleLabel.setAlignment(Pos.CENTER);
-
         titleBox.getChildren().add(titleLabel);
 
-        HBox nameBox = new HBox();
-        nameBox.setSpacing(10);
-        nameBox.setAlignment(Pos.CENTER_RIGHT);
+        ComboBox<RestaurantPhoneBook> restaurantsComboBox = new ComboBox<>();
+        restaurantsComboBox.setPrefWidth(MAX_VALUE);
+        restaurantsComboBox.setPromptText("Select restaurant");
+        Label restaurantsLabel = new Label("Restaurants:");
+        restaurantsLabel.setFont(largeFont);
+        restaurantsLabel.setLabelFor(restaurantsComboBox);
+        HBox restaurantsBox = new HBox();
+        restaurantsBox.setAlignment(Pos.CENTER);
+        restaurantsBox.getChildren().addAll(restaurantsLabel, restaurantsComboBox);
 
-        Label nameLabel = new Label("Name:");
-        nameLabel.setFont(smallFont);
-        nameLabel.setLabelFor(nameField);
-        nameLabel.setAlignment(Pos.CENTER_RIGHT);
-
-        nameField = new TextField();
+        TextField nameField = new TextField();
         nameField.setPromptText("Restaurant name");
-
+        Label nameLabel = new Label("Name:");
+        nameLabel.setLabelFor(nameField);
+        HBox nameBox = new HBox();
         nameBox.getChildren().addAll(nameLabel, nameField);
 
-        HBox cuisineBox = new HBox();
-        cuisineBox.setSpacing(10);
-        cuisineBox.setAlignment(Pos.TOP_LEFT);
-
-        Label cuisineLabel = new Label("Cuisine:");
-        cuisineLabel.setFont(smallFont);
-        cuisineLabel.setLabelFor(cuisineField);
-        cuisineLabel.setAlignment(Pos.CENTER_RIGHT);
-
-        cuisineField = new ComboBox<>();
-        cuisineField.getItems()
-                .addAll("Chinese", "Italian", "Japanese", "Korean", "Mexican", "Thai", "Vietnamese");
-        cuisineField.setPromptText("Select cuisine");
-
-        cuisineBox.getChildren().addAll(cuisineLabel, cuisineField);
-
-        HBox phoneNumberBox = new HBox();
-        phoneNumberBox.setSpacing(10);
-        phoneNumberBox.setAlignment(Pos.CENTER_RIGHT);
-
-        Label phoneNumberLabel = new Label("Phone Number:");
-        phoneNumberLabel.setFont(smallFont);
-        phoneNumberLabel.setLabelFor(phoneNumberField);
-        phoneNumberLabel.setAlignment(Pos.CENTER_RIGHT);
-
-        phoneNumberField = new TextField();
+        TextField phoneNumberField = new TextField();
         phoneNumberField.setPromptText("Insert number");
-
+        Label phoneNumberLabel = new Label("Phone Number:");
+        phoneNumberLabel.setLabelFor(phoneNumberField);
+        HBox phoneNumberBox = new HBox();
         phoneNumberBox.getChildren().addAll(phoneNumberLabel, phoneNumberField);
 
-        HBox tablesBox = new HBox();
-        tablesBox.setAlignment(Pos.CENTER_RIGHT);
-        tablesBox.setSpacing(10);
+        HBox nameAndPhoneBox = new HBox();
+        nameAndPhoneBox.getChildren().addAll(nameBox, phoneNumberBox);
 
-        Label tablesLabel = new Label("Tables:");
-        tablesLabel.setFont(smallFont);
-        tablesLabel.setLabelFor(tablesField);
-        tablesLabel.setAlignment(Pos.CENTER_RIGHT);
-
-        tablesField = new TextField();
-        tablesField.setPromptText("Number of tables");
-
-        tablesBox.getChildren().addAll(tablesLabel, tablesField);
-
-        HBox ratingBox = new HBox();
-        ratingBox.setAlignment(Pos.BOTTOM_RIGHT);
-        ratingBox.setSpacing(10);
-
-        Label ratingLabel = new Label("Rating:");
-        ratingLabel.setFont(smallFont);
-        ratingLabel.setLabelFor(ratingField);
-        ratingLabel.setAlignment(Pos.CENTER_RIGHT);
-
-        ratingField = new TextField();
-        ratingField.setPromptText("1.0 - 5.0");
-
-        ratingBox.getChildren().addAll(ratingLabel, ratingField);
-
-        VBox addressBox = new VBox();
-        addressBox.setAlignment(Pos.CENTER_LEFT);
-        addressBox.setSpacing(10);
-
-        Label addressLabel = new Label("Address:");
-        addressLabel.setFont(smallFont);
-        addressLabel.setLabelFor(addressField);
-
-        addressField = new TextArea();
+        TextArea addressField = new TextArea();
         addressField.setPromptText("Restaurant address");
         addressField.setPrefRowCount(3);
         addressField.setPrefColumnCount(20);
-
+        Label addressLabel = new Label("Address:");
+        addressLabel.setLabelFor(addressField);
+        VBox addressBox = new VBox();
         addressBox.getChildren().addAll(addressLabel, addressField);
 
-        HBox establishedBox = new HBox();
-        establishedBox.setSpacing(10);
-        establishedBox.setAlignment(Pos.CENTER_RIGHT);
+        Button saveButton = new Button("Save");
 
-        Label establishedLabel = new Label("Established:");
-        establishedLabel.setFont(smallFont);
-        establishedLabel.setLabelFor(establishedField);
-        establishedLabel.setAlignment(Pos.CENTER_LEFT);
-
-        establishedField = new DatePicker();
-        establishedField.setPromptText("Date of establishment");
-
-        establishedBox.getChildren().addAll(establishedLabel, establishedField);
-
-        HBox wheelchairAccessibleBox = new HBox();
-        wheelchairAccessibleBox.setSpacing(10);
-        wheelchairAccessibleBox.setAlignment(Pos.CENTER_LEFT);
-
-        Label wheelchairAccessibleLabel = new Label("Wheelchair Accessible:");
-        wheelchairAccessibleLabel.setFont(smallFont);
-        wheelchairAccessibleLabel.setLabelFor(wheelchairAccessibleField);
-
-        wheelchairAccessibleField = new CheckBox();
-
-        wheelchairAccessibleBox.getChildren().addAll(wheelchairAccessibleLabel, wheelchairAccessibleField);
-
-        VBox vContainerLeft = new VBox();
-        vContainerLeft.setSpacing(10);
-        vContainerLeft.setAlignment(Pos.CENTER_LEFT);
-        vContainerLeft.setPrefWidth(MAX_VALUE);
-        vContainerLeft.getChildren().addAll(nameBox, phoneNumberBox, tablesBox, ratingBox, establishedBox);
-
-        VBox vContainerRight = new VBox();
-        vContainerRight.setAlignment(Pos.CENTER_RIGHT);
-        vContainerRight.setSpacing(10);
-        vContainerRight.setPrefWidth(MAX_VALUE);
-        vContainerRight.getChildren().addAll(cuisineBox, addressBox, wheelchairAccessibleBox);
-
-        VBox listContainer = new VBox();
-
-        restaurantContactListView = new ListView<>();
+        ListView<RestaurantContact> restaurantContactListView = new ListView<>();
         restaurantContactListView.setPrefHeight(100);
+        restaurantContactListView.setPrefWidth(MAX_VALUE);
         restaurantContactListView.setPlaceholder(new Label("No restaurant contacts added yet."));
 
+        Button newButton = new Button("New");
+        Button deleteButton = new Button("Delete");
+        Button restaurantsViewButton = new Button("Go to restaurants");
         HBox listButtonBox = new HBox();
-        listButtonBox.setSpacing(10);
+        listButtonBox.getChildren().addAll(deleteButton, newButton, restaurantsViewButton);
 
-        deleteButton = new Button("Delete");
-        deleteButton.setPrefWidth(MAX_VALUE);
-        deleteButton.setFont(smallFont);
-        deleteButton.setAlignment(Pos.CENTER);
+        VBox mainPanel = new VBox();
+        mainPanel.setSpacing(10);
+        mainPanel.setPadding(new Insets(10));
+        mainPanel.setPrefHeight(MAX_VALUE);
+        mainPanel.setPrefWidth(MAX_VALUE);
+        mainPanel.getChildren().addAll(titleBox, restaurantsBox, nameAndPhoneBox, addressBox, saveButton,
+                restaurantContactListView, listButtonBox);
 
-        newButton = new Button("New");
-        newButton.setPrefWidth(MAX_VALUE);
-        newButton.setFont(smallFont);
-        newButton.setAlignment(Pos.CENTER);
+        mainPanel.getChildren().forEach(node -> {
+            if (node instanceof Button) {
+                ((Button) node).setPrefWidth(MAX_VALUE);
+                ((Button) node).setFont(smallFont);
+            }
+            if (node instanceof HBox) {
+                ((HBox) node).setAlignment(Pos.CENTER);
+                ((HBox) node).setSpacing(10);
 
-        listButtonBox.getChildren().addAll(deleteButton, newButton);
-        listContainer.getChildren().addAll(restaurantContactListView, listButtonBox);
+                ((HBox) node).getChildren().forEach(child -> {
+                    if (child instanceof Button) {
+                        ((Button) child).setPrefWidth(MAX_VALUE);
+                        ((Button) child).setFont(smallFont);
+                    }
+                    if (child instanceof Label) {
+                        ((Label) child).setFont(smallFont);
+                        ((Label) child).setAlignment(Pos.CENTER_LEFT);
+                        ((Label) child).setMinWidth(100);
+                    }
 
-        cancelButton = new Button("Cancel");
-        cancelButton.setPrefWidth(MAX_VALUE);
-        cancelButton.setFont(smallFont);
-        cancelButton.setAlignment(Pos.CENTER);
+                    if (child instanceof HBox){
+                        ((HBox) child).setAlignment(Pos.CENTER);
+                        ((HBox) child).setSpacing(10);
+                        ((HBox) child).getChildren().forEach(child2 -> {
+                            if (child2 instanceof Label) {
+                                ((Label) child2).setFont(smallFont);
+                                ((Label) child2).setAlignment(Pos.CENTER_LEFT);
+                                ((Label) child2).setMinWidth(120);
+                            }
+                            if (child2 instanceof TextField) {
+                                ((TextField) child2).setAlignment(Pos.CENTER);
+                                ((TextField) child2).setMinWidth(130);
+                                ((TextField) child2).setPrefWidth(MAX_VALUE);
+                            }
+                        });
+                    }
+                });
+            }
+        });
 
-        saveButton = new Button("Save");
-        saveButton.setPrefWidth(MAX_VALUE);
-        saveButton.setFont(smallFont);
-        saveButton.setAlignment(Pos.CENTER);
-
-        mainPanel.add(titleBox, 0, 0, 2, 1);
-        mainPanel.add(vContainerLeft, 0, 1);
-        mainPanel.add(vContainerRight, 1, 1);
-        mainPanel.add(saveButton, 0, 2, 2, 1);
-        mainPanel.add(listContainer, 0, 3, 2, 1);
-        mainPanel.add(cancelButton, 0, 4, 2, 1);
+        titleLabel.setFont(largeFont);
 
         root = mainPanel;
     }
--- src/practicumopdracht/views/RestaurantPhoneBookView.java
+++ src/practicumopdracht/views/RestaurantPhoneBookView.java
@@ -2,10 +2,7 @@ package practicumopdracht.views;
 
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
@@ -16,23 +13,14 @@ import static java.lang.Integer.MAX_VALUE;
 
 public class RestaurantPhoneBookView extends View {
 
-    private TextField countryField;
-    private TextField cityField;
-    private ListView<RestaurantPhoneBook> restaurantPhoneBookList;
-    private Button saveButton;
-    private Button deleteButton;
-    private Button newButton;
-    private Button selectButton;
-
     @Override
     protected void initializeView() {
-        VBox mainPanel = new VBox();
-        mainPanel.setSpacing(10);
-        mainPanel.setAlignment(Pos.CENTER);
-        mainPanel.setPadding(new Insets(10));
-
         Font smallFont = Font.font("Helvetica", FontWeight.BLACK, 16);
         Font largeFont = Font.font("Helvetica", FontWeight.BOLD, 36);
+
+        Label titleLabel = new Label("Restaurant Phone Book");
+        titleLabel.setAlignment(Pos.CENTER);
+        titleLabel.setFont(largeFont);
 
         HBox titleBox = new HBox();
         titleBox.setBorder(new Border(
@@ -40,77 +28,132 @@ public class RestaurantPhoneBookView extends View {
                         Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT
                 )));
         titleBox.setAlignment(Pos.TOP_CENTER);
-
-        Label titleLabel = new Label("Restaurant Phone Book");
-        titleLabel.setAlignment(Pos.CENTER);
-        titleLabel.setFont(largeFont);
-
         titleBox.getChildren().add(titleLabel);
+        TextField nameField = new TextField();
+        nameField.setPromptText("Restaurant Name");
 
-        HBox countryAndCityBox = new HBox();
-        countryAndCityBox.setSpacing(10);
+        Label nameLabel = new Label("Name:");
+        nameLabel.setLabelFor(nameField);
+        nameLabel.setAlignment(Pos.CENTER_LEFT);
 
-        HBox countryBox = new HBox();
-        countryBox.setSpacing(10);
-        countryBox.setAlignment(Pos.CENTER_LEFT);
+        HBox nameBox = new HBox();
+        nameBox.getChildren().addAll(nameLabel, nameField);
 
-        Label countryLabel = new Label("Country:");
-        countryLabel.setFont(smallFont);
-        countryLabel.setLabelFor(countryField);
-        countryLabel.setMinWidth(80);
+        TextField cuisineField = new TextField();
+        cuisineField.setPromptText("Cuisine");
+        cuisineField.setPrefWidth(MAX_VALUE);
 
-        countryField = new TextField();
-        countryField.setPromptText("Country");
-        countryField.setPrefWidth(MAX_VALUE);
+        Label cuisineLabel = new Label("Cuisine:");
+        cuisineLabel.setLabelFor(cuisineField);
 
-        countryBox.getChildren().addAll(countryLabel, countryField);
+        HBox cuisineBox = new HBox();
+        cuisineBox.getChildren().addAll(cuisineLabel, cuisineField);
 
-        HBox cityBox = new HBox();
-        cityBox.setSpacing(10);
-        cityBox.setAlignment(Pos.CENTER_RIGHT);
+        TextField ratingField = new TextField();
+        ratingField.setPromptText("Restaurant Rating (0-10.0))");
+        ratingField.setPrefWidth(MAX_VALUE);
 
-        Label cityLabel = new Label("City:");
-        cityLabel.setFont(smallFont);
-        cityLabel.setLabelFor(cityField);
-        cityLabel.setMinWidth(50);
+        Label ratingLabel = new Label("Rating:");
+        ratingLabel.setLabelFor(ratingField);
 
-        cityField = new TextField();
-        cityField.setPromptText("City");
-        cityField.setPrefWidth(MAX_VALUE);
+        HBox ratingBox = new HBox();
+        ratingBox.getChildren().addAll(ratingLabel, ratingField);
 
-        cityBox.getChildren().addAll(cityLabel, cityField);
+        TextField tablesField = new TextField();
+        tablesField.setPromptText("City");
 
-        countryAndCityBox.getChildren().addAll(countryBox, cityBox);
+        Label tablesLabel = new Label("Tables:");
+        tablesLabel.setLabelFor(tablesField);
 
-        saveButton = new Button("Save");
+        HBox tablesBox = new HBox();
+        tablesBox.getChildren().addAll(tablesLabel, tablesField);
+
+        DatePicker establishedField = new DatePicker();
+        establishedField.setPromptText("Date of establishment");
+
+        Label establishedLabel = new Label("Established:");
+        establishedLabel.setLabelFor(establishedField);
+
+        HBox establishedBox = new HBox();
+        establishedBox.getChildren().addAll(establishedLabel, establishedField);
+
+        CheckBox wheelchairAccessibleField = new CheckBox();
+
+        Label wheelchairAccessibleLabel = new Label("Wheelchair Accessible:");
+        wheelchairAccessibleLabel.setLabelFor(wheelchairAccessibleField);
+
+        HBox wheelchairAccessibleBox = new HBox();
+        wheelchairAccessibleBox.getChildren().addAll(wheelchairAccessibleLabel, wheelchairAccessibleField);
+
+        GridPane restaurantDetailsGrid = new GridPane();
+        restaurantDetailsGrid.setHgap(10);
+        restaurantDetailsGrid.setVgap(10);
+        restaurantDetailsGrid.setPadding(new Insets(10));
+        restaurantDetailsGrid.add(nameBox, 0, 0);
+        restaurantDetailsGrid.add(cuisineBox, 1, 0);
+        restaurantDetailsGrid.add(tablesBox, 0, 1);
+        restaurantDetailsGrid.add(ratingBox, 1, 1);
+        restaurantDetailsGrid.add(establishedBox, 0, 2);
+        restaurantDetailsGrid.add(wheelchairAccessibleBox, 1, 2);
+
+        restaurantDetailsGrid.getChildren().forEach(node -> {
+            if (node instanceof HBox) {
+                ((HBox) node).setSpacing(10);
+                ((HBox) node).setAlignment(Pos.CENTER_LEFT);
+
+                ((HBox) node).getChildren().forEach(child -> {
+                    if (child instanceof Label) {
+                        ((Label) child).setMinWidth(100);
+                        ((Label) child).setFont(smallFont);
+                        ((Label) child).setAlignment(Pos.CENTER_LEFT);
+                    }
+                    if (child instanceof TextField) {
+                        ((TextField) child).setMinWidth(130);
+                        ((TextField) child).setPrefWidth(MAX_VALUE);
+                    }
+                    if (child instanceof DatePicker) {
+                        ((DatePicker) child).setMinWidth(100);
+                        ((DatePicker) child).setPrefWidth(MAX_VALUE);
+                    }
+                    if (child instanceof CheckBox) {
+                        ((CheckBox) child).setMinWidth(50);
+                    }
+                });
+            }
+        });
+
+        Button saveButton = new Button("Save");
         saveButton.setFont(smallFont);
         saveButton.setPrefWidth(MAX_VALUE);
 
-        VBox listContainer = new VBox();
-
-        restaurantPhoneBookList = new ListView<>();
+        ListView<RestaurantPhoneBook> restaurantPhoneBookList = new ListView<>();
         restaurantPhoneBookList.setPlaceholder(new Label("No restaurant phone books added yet"));
 
-        HBox listButtonBox = new HBox();
-        listButtonBox.setSpacing(10);
-
-        newButton = new Button("New");
+        Button newButton = new Button("New");
         newButton.setFont(smallFont);
         newButton.setPrefWidth(MAX_VALUE);
 
-        deleteButton = new Button("Delete");
+        Button deleteButton = new Button("Delete");
         deleteButton.setFont(smallFont);
         deleteButton.setPrefWidth(MAX_VALUE);
 
+        HBox listButtonBox = new HBox();
+        listButtonBox.setSpacing(10);
         listButtonBox.getChildren().addAll(deleteButton, newButton);
 
+        VBox listContainer = new VBox();
         listContainer.getChildren().addAll(restaurantPhoneBookList, listButtonBox);
 
-        selectButton = new Button("Select");
+        Button selectButton = new Button("Select");
         selectButton.setFont(smallFont);
         selectButton.setPrefWidth(MAX_VALUE);
 
-        mainPanel.getChildren().addAll(titleBox, countryAndCityBox, saveButton, listContainer, selectButton);
+        VBox mainPanel = new VBox();
+        mainPanel.setSpacing(10);
+        mainPanel.setAlignment(Pos.CENTER);
+        mainPanel.setPadding(new Insets(10));
+        mainPanel.getChildren().addAll(titleBox, restaurantDetailsGrid,
+                saveButton, listContainer, selectButton);
 
         root = mainPanel;
     }
